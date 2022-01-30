ebcb374c7a5a0ce7b709821040d201be65a5cb46
alfredoaguiararce <67090401+alfredoaguiararce@users.noreply.github.com>
2022-01-29 23:34:47 -0700

    Agregue el codigo de condicionales.py

---
+++ programas/condicionales.py
@@ -0,0 +1,20 @@
+# Parte 1 detectar si una persona es mayor de edad.
+
+# NOTA : puedes des comentar el codigo o comentarlo, seleccionando las lineas que deseas comentar/descomentar y utilizando el comando 'Ctrl + }'
+# edad = int(input("Escribe tu edad : "))
+
+# if edad > 17:
+#     print("Eres mayor de edad.")
+# else:
+#     print("Eres menor de edad.")
+
+# Parte 2 Detectar si un numero es mayor, menor o igual a 5.
+
+numero = int(input("Escribe un numero : "))
+
+if numero > 5:
+    print("Es mayor a 5.")
+elif numero == 5:
+    print("Es igual a 5.")
+else:
+    print("Es menor a 5.")
